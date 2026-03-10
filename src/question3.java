29e5a406a7bea28cdc43c40b51b5860416d9d606
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-10 09:33:32 +0800

    question 3, cgpt classification

--- src/question3.java
+++ src/question3.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
 public class question3 {
-
+//cgpt
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         int[] target = new int[n];
 
         for(int i = 0; i < n; i++) {
