ecaff13d2b8e054cf362a98b8481dfe85b230140
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-10 09:32:41 +0800

    question 3

---
+++ src/question3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class question3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] target = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            target[i] = sc.nextInt();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder program = new StringBuilder();
+
+        for(int value : target){
+
+            stack.push(1);
+            program.append("1");
+
+            while(stack.peek() < value){
+
+                int top = stack.peek();
+
+                stack.push(top + 13);
+                program.append("d");
+
+                if(stack.size() >= 2){
+
+                    int a = stack.pop();
+                    int b = stack.pop();
+
+                    stack.push(a + b);
+                    program.append("+");
+                }
+            }
+        }
+        System.out.println(program.toString());
+    }
+}
