d65d2dff6b28edbe8c90a1640861bbcfcfaa3fc9
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-10 10:29:28 +0800

    perbaikan no 4

--- src/question4.java
+++ src/question4.java
@@ -1,34 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
-//cgpt
 public class question4 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Masukkan jumlah angka:");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
         System.out.println("Masukkan angka:");
+        String line = sc.nextLine();
+
+        String[] input = line.split(" ");
+        int n = input.length;
+
+        int[] numbers = new int[n];
 
         for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
+            numbers[i] = Integer.parseInt(input[i]);
         }
+
+        // ORDER SYNC PROTOCOL
+        Arrays.sort(numbers);
+
+        System.out.println("KASMIR TAMPAN");
 
         Stack<Integer> stack = new Stack<>();
 
-        for(int num : arr){
-
-            while(!stack.isEmpty() && stack.peek() > num){
-                System.out.print(stack.pop() + " ");
-            }
-
+        for(int num : numbers){
             stack.push(num);
         }
+
+        System.out.println("KASMIR GANTENG BANGET");
 
         Stack<Integer> temp = new Stack<>();
 
